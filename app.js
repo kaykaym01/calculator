d39878cae2719436a6205c8ce324f91b6667f06f
kaymoore <kaymoore@macbook>
2022-04-30 09:20:39 -0400

    Add operate method to take operator, two numbers, and produce answer

--- app.js
+++ app.js
@@ -13,3 +13,22 @@ function add(a, b){
 function subtract(a,b) {
     return a-b;
 }
+
+function operate(operator, a, b){
+    let answer;
+    switch(operator){
+        case "*":
+            answer = multiply(a,b);
+            break;
+        case "/":
+            answer = divide(a,b);
+            break;
+        case "+":
+            answer = add(a,b);
+            break;
+        case("-"):
+            answer = subtract(a,b);
+            break;
+    }
+    return answer;
+}
